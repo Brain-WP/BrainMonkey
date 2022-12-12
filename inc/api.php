70e95a3cd2bfd8bb0dd46084b10176ef3306e2bf
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2022-12-12 19:48:58 +0100

    Stub _n_noop, _nx_noop, translate_nooped_plural
    Stub for those were missing in stubTranslationFunctions()
    
    See #51

--- inc/api.php
+++ inc/api.php
@@ -155,6 +155,15 @@ namespace Brain\Monkey\Functions {
                 'esc_attr_x' => [EscapeHelper::class, 'esc'],
                 'esc_html_e' => [EscapeHelper::class, 'escAndEcho'],
                 'esc_attr_e' => [EscapeHelper::class, 'escAndEcho'],
+                '_n_noop'    => static function ($singular, $plural) {
+                    return compact('singular', 'plural');
+                },
+                '_nx_noop' => static function ($singular, $plural) {
+                    return compact('singular', 'plural');
+                },
+                'translate_nooped_plural' => static function($nooped, $number) {
+                    return ($number === 1) ? $nooped['singular'] : $nooped['plural'];
+                },
             ]
         );
 
